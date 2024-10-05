95e0542f51ca9fb0663bc5bd0719b8baf5d2534c
Nan Xiao <me@nanx.me>
2024-10-05 17:30:52 -0400

    Use Cascadia Code

--- images/banner.sh
+++ images/banner.sh
@@ -4,12 +4,12 @@ magick -size 2048x734 \
   -define gradient:angle=330 \gradient:#03448c-#17ffc6 \
   -gravity center \
   -pointsize 105 \
-  -font 'Jetbrains-Mono-Bold' \
+  -font 'Cascadia-Code-SemiBold' \
   -fill white \
   -annotate +0-100 'r-base-shortcuts' \
   -pointsize 28 \
-  -font 'Jetbrains-Mono-SemiBold' \
+  -font 'Cascadia-Code-SemiBold' \
   -annotate +0+50 'Object Generation · Object Transformation · Vectorization' \
   -annotate +0+100 'List Operations · Conditional Logic · Argument Handling' \
   -annotate +0+150 'Side-Effect Management · Numerical Computations' \
-  png:- | pngquant - --force --output banner.png
+  png:- | pngquant - --force --output images/banner.png
